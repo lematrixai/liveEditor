9c38ad20950f67325238a68afb5026d0cac928d1
lemajr <erickbale360@gmail.com>
2025-03-09 21:19:18 +0300

    Just deployment test mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
-import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
   }
 };
 
+export default nextConfig;
