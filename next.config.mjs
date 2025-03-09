144e3430847925ead37237201e53b2e629e7ad8b
lemajr <erickbale360@gmail.com>
2025-03-09 20:03:00 +0300

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import {withSentryConfig} from '@sentry/nextjs';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+  }
+};
+
